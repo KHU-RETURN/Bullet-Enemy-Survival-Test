94a29476f46339d2ae9b8bc1f43692af8fbe7959
정우빈 <bunaro4501@naver.com>
2022-08-24 02:54:21 +0900

    이미지 수정
    터렛의 이미지를 전체적으로 수정하고 움직임을 추가했습니다

--- swing_knife.py
+++ swing_knife.py
@@ -3,9 +3,11 @@ import sys
 import os
 import random
 import math
+import button
 
 pygame.init()
 
+start_game = False
 screen_width = 800
 screen_height = 640
 size = [screen_width, screen_height]  # 창 크기
@@ -25,6 +27,8 @@ myFont = pygame.font.Font('data1/a옛날목욕탕B.ttf', 30)
 game_over_font = pygame.font.Font('data1/a옛날목욕탕B.ttf', 90)
 
 # 이미지 설정
+start_img = pygame.image.load('data1/start_btn.png').convert_alpha()
+exit_img = pygame.image.load('data1/exit_btn.png').convert_alpha()
 background_img = pygame.image.load('data1/img/bg/background.png').convert() #convert, convert_alpha 차이
 sword_img = pygame.image.load('data1/img/player/swing/sword.png').convert_alpha()
 sword_img = pygame.transform.scale(sword_img, (int(sword_img.get_width()*1.2), int(sword_img.get_height()*0.3)))
@@ -166,8 +170,7 @@ class Spin(pygame.sprite.Sprite): #애니메이션 고치는중, 뒤에 update_a
         self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        screen.blit(pygame.transform.flip(
-            self.image, self.flip, False), self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
         for enemy in enemy_group:
             if enemy.rect.colliderect(self.rect):
@@ -180,7 +183,7 @@ class Spin(pygame.sprite.Sprite): #애니메이션 고치는중, 뒤에 update_a
                     print("score:", player.score)
 
     def update_animation(self):
-        ANIM_COOLDOWN = 10
+        ANIM_COOLDOWN = 100
 
         self.image = self.animation_list[self.action][self.frame_index]
 
@@ -197,6 +200,9 @@ class Spin(pygame.sprite.Sprite): #애니메이션 고치는중, 뒤에 update_a
 
             self.frame_index = 0
             self.update_time = pygame.time.get_ticks()
+
+    def draw(self, display): # 플레이어 화면에 생성
+        display.blit(pygame.transform.flip(self.image, self.flip, False), self.rect) #transform? 왜사용?
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, dir_x, dir_y, type):
@@ -489,7 +495,11 @@ enemy_group = pygame.sprite.Group()
 
 player = Player(400, 320, 1.5, 3)
 
-# 변수 설정
+# 버튼 만들기
+start_button = button.Button(screen_width // 2 - 130, screen_height // 2 -150, start_img, 1)
+exit_button = button.Button(screen_width // 2 -130, screen_height // 2 +50, exit_img, 1)
+
+# 변수 설정a
 
 moving_left = False
 moving_right = False
@@ -516,134 +526,147 @@ game_run = True
 while game_run:
     clock.tick(60) # FPS 설정, 1초에 60번 while문이 돌도록
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-
-    attack_cooltime += dt
-    spawn_cooltime += dt
-
-    screen.fill(background_color) # 배경
-    screen.blit(background_img, (0, 0)) # 백그라운드 이미지
-
-    # 그룹 업데이트
-    bullet_group.update()
-    bullet_group.draw(screen) # 총알이 제일 뒤로 보이도록 먼저 그림
-
-    if player.drawing:
-        player.update_animation()
-        player.draw(screen) # 플레이어 생성
-
-    if player.alive:
-        enemy_group.update()
-
-    if player.lives == 3:
-        screen.blit(pygame.transform.flip(heart, False, False), (0, 0))
-        screen.blit(pygame.transform.flip(heart, False, False), (30, 0))
-        screen.blit(pygame.transform.flip(heart, False, False), (60, 0))
-    elif player.lives == 2:
-        screen.blit(pygame.transform.flip(heart, False, False), (0, 0))
-        screen.blit(pygame.transform.flip(heart, False, False), (30, 0))
-        screen.blit(pygame.transform.flip(empty_heart, False, False), (60, 0))
-    elif player.lives == 1:
-        screen.blit(pygame.transform.flip(heart, False, False), (0, 0))
-        screen.blit(pygame.transform.flip(empty_heart, False, False), (30, 0))
-        screen.blit(pygame.transform.flip(empty_heart, False, False), (60, 0))
-    else:
-        screen.blit(pygame.transform.flip(empty_heart, False, False), (0, 0))
-        screen.blit(pygame.transform.flip(empty_heart, False, False), (30, 0))
-        screen.blit(pygame.transform.flip(empty_heart, False, False), (60, 0))
-
-    for enemy in enemy_group:
-        enemy.update_animation()
-        enemy.draw(screen)
+    if start_game == False:
+        # 메뉴 표시
+        screen.fill(background_color)
+        # 버튼 추가
+        if start_button.draw(screen):
+            start_game = True
+        if exit_button.draw(screen):
+            game_run = False
     
-    # 화면에 점수 표시
-    score_text = myFont.render("Score: " + str(player.score), True, (255, 255, 255))
-    screen.blit(score_text, [350, 0])
+    else:
 
-    # 플레이어 생존 시
-    if player.alive:
+        mouse_x, mouse_y = pygame.mouse.get_pos()
 
-        if clicking and attack_cooltime >= attack_rate:
-            sword_s.play()
-            spin = Spin(player.rect.centerx, player.rect.centery, 5, 'spin')
-            attack_cooltime = 0
+        attack_cooltime += dt
+        spawn_cooltime += dt
 
-        if moving_left or moving_right or moving_up or moving_down:
-            player.update_action(1) # run 애니메이션으로 전환
+        screen.fill(background_color) # 배경
+        screen.blit(background_img, (0, 0)) # 백그라운드 이미지
+
+        # 그룹 업데이트
+        bullet_group.update()
+        bullet_group.draw(screen) # 총알이 제일 뒤로 보이도록 먼저 그림
+
+        if player.drawing:
+            player.update_animation()
+            player.draw(screen) # 플레이어 생성
+
+        if player.alive:
+            enemy_group.update()
+
+        if player.lives == 3:
+            screen.blit(pygame.transform.flip(heart, False, False), (0, 0))
+            screen.blit(pygame.transform.flip(heart, False, False), (30, 0))
+            screen.blit(pygame.transform.flip(heart, False, False), (60, 0))
+        elif player.lives == 2:
+            screen.blit(pygame.transform.flip(heart, False, False), (0, 0))
+            screen.blit(pygame.transform.flip(heart, False, False), (30, 0))
+            screen.blit(pygame.transform.flip(empty_heart, False, False), (60, 0))
+        elif player.lives == 1:
+            screen.blit(pygame.transform.flip(heart, False, False), (0, 0))
+            screen.blit(pygame.transform.flip(empty_heart, False, False), (30, 0))
+            screen.blit(pygame.transform.flip(empty_heart, False, False), (60, 0))
         else:
-            player.update_action(0) # idle 애니메이션으로 전환
-        player.move(moving_left, moving_right, moving_up, moving_down)
+            screen.blit(pygame.transform.flip(empty_heart, False, False), (0, 0))
+            screen.blit(pygame.transform.flip(empty_heart, False, False), (30, 0))
+            screen.blit(pygame.transform.flip(empty_heart, False, False), (60, 0))
 
-        if mouse_x < player.rect.x:
-            player.flip = True
-            player.direction = -1
-        else:
-            player.flip = False
-            player.direction = 1
+        for enemy in enemy_group:
+            enemy.update_animation()
+            enemy.draw(screen)
+        
+        # 화면에 점수 표시
+        score_text = myFont.render("Score: " + str(player.score), True, (255, 255, 255))
+        screen.blit(score_text, [350, 0])
 
-        # 충돌처리
-        for bullet in bullet_group:
-            if bullet.rect.colliderect(player.rect):
-                if bullet.type == "enemy_bullet":
-                    bullet.kill()
+        # 플레이어 생존 시
+        if player.alive:
+
+            if clicking and attack_cooltime >= attack_rate:
+                sword_s.play()
+                spin = Spin(player.rect.centerx, player.rect.centery, 5, 'spin')
+                attack_cooltime = 0
+                spin.update_animation()
+                spin.draw(screen) # 플레이어 생성
+
+            if moving_left or moving_right or moving_up or moving_down:
+                player.update_action(1) # run 애니메이션으로 전환
+            else:
+                player.update_action(0) # idle 애니메이션으로 전환
+            player.move(moving_left, moving_right, moving_up, moving_down)
+
+            if mouse_x < player.rect.x:
+                player.flip = True
+                player.direction = -1
+            else:
+                player.flip = False
+                player.direction = 1
+
+            # 충돌처리
+            for bullet in bullet_group:
+                if bullet.rect.colliderect(player.rect):
+                    if bullet.type == "enemy_bullet":
+                        bullet.kill()
+                        player_hit_s.play()
+                        player.lives -= 1
+                        print("life:", player.lives)
+
+                        if player.lives <= 0:
+                            print("Game Over")
+                            death_s.set_volume(0.05)
+                            death_s.play()
+                            player.update_action(2)
+                            fade_in = 10
+                            player.alive = False
+
+            for enemy in enemy_group:
+                if enemy.rect.colliderect(player.rect):
                     player_hit_s.play()
                     player.lives -= 1
+                    enemy.kill()
                     print("life:", player.lives)
-
                     if player.lives <= 0:
                         print("Game Over")
                         death_s.set_volume(0.05)
                         death_s.play()
-                        player.update_action(2)
+                        player.update_action(2) # death 애니메이션으로 전환
                         fade_in = 10
                         player.alive = False
 
-        for enemy in enemy_group:
-            if enemy.rect.colliderect(player.rect):
-                player_hit_s.play()
-                player.lives -= 1
-                enemy.kill()
-                print("life:", player.lives)
-                if player.lives <= 0:
-                    print("Game Over")
-                    death_s.set_volume(0.05)
-                    death_s.play()
-                    player.update_action(2) # death 애니메이션으로 전환
-                    fade_in = 10
-                    player.alive = False
+        if not player.alive and fade_in > 0:
+            fade_in -= 0.1
+            black_surf = pygame.Surface((screen_width, screen_height))
+            black_surf.set_alpha(int(255*fade_in/10))
+            screen.blit(black_surf, (0, 0))
+        elif not player.alive and fade_in <= 0:
+            screen.blit(pygame.Surface((screen_width, screen_height)), (0, 0))
+            game_over_text = game_over_font.render("Game Over ", True, (255, 0, 0))
+            press_r_text = myFont.render(
+                "press R to replay", True, (255, 255, 255))
+            score_text = myFont.render(
+                "Score: " + str(player.score), True, (255, 255, 255))
 
-    if not player.alive and fade_in > 0:
-        fade_in -= 0.1
-        black_surf = pygame.Surface((screen_width, screen_height))
-        black_surf.set_alpha(int(255*fade_in/10))
-        screen.blit(black_surf, (0, 0))
-    elif not player.alive and fade_in <= 0:
-        screen.blit(pygame.Surface((screen_width, screen_height)), (0, 0))
-        game_over_text = game_over_font.render("Game Over ", True, (255, 0, 0))
-        press_r_text = myFont.render(
-            "press R to replay", True, (255, 255, 255))
-        score_text = myFont.render(
-            "Score: " + str(player.score), True, (255, 255, 255))
+            screen.blit(game_over_text, [
+                        screen_width/2 - 250, screen_height/2 - 100])
+            screen.blit(press_r_text, [screen_width/2 - 80, screen_height/2])
+            screen.blit(score_text, [350, 0])
 
-        screen.blit(game_over_text, [
-                    screen_width/2 - 250, screen_height/2 - 100])
-        screen.blit(press_r_text, [screen_width/2 - 80, screen_height/2])
-        screen.blit(score_text, [350, 0])
+        # 적 소환
+        if spawn_cooltime >= spawn_rate and player.alive:
+            if player.score < 500:
+                spawn_rate = random.randint(100, 200)
+            elif player.score < 700:
+                spawn_rate = random.randint(80, 180)
+            elif player.score < 1000:
+                spawn_rate = random.randint(60, 160)
+            else:
+                spawn_rate = random.randint(50, 100)
 
-    # 적 소환
-    if spawn_cooltime >= spawn_rate and player.alive:
-        if player.score < 500:
-            spawn_rate = random.randint(100, 200)
-        elif player.score < 700:
-            spawn_rate = random.randint(80, 180)
-        elif player.score < 1000:
-            spawn_rate = random.randint(60, 160)
-        else:
-            spawn_rate = random.randint(50, 100)
-
-        spawn_enemy()
-        spawn_cooltime = 0
-    
+            spawn_enemy()
+            spawn_cooltime = 0
+        
     # 각종 입력 감지
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
